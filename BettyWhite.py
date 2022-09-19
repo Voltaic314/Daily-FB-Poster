a246fadb4b2180d9ac775416bdec43117cbb04c1
Logan Maupin <104940692+Voltaic314@users.noreply.github.com>
2022-09-19 01:00:37 -0400

    Heavily refactored & now just posts she's dead
    As the title suggests, I refactored the hell out of this code to add in a lot more formal formatting. 
    
    but also on top of that, I modified the code to no longer use the list of 9 possible statuses like it was before. Personally just because I thought it was getting very repetitive over and over. (How many ways can you say someone is dead in a funny way?). 
    
    Now it will just say "She's Dead." every day like it used to before. Anyway, I'm pretty much done with this project but wanted to give it one final touch up before letting it stay automated and never touching the code again. It's been fun. Time to move onto bigger and better things. 
    
    Truth be told I don't even touch meme bot or nature bot anymore either. But I have nothing to add to them without doing a BUNCH more code to them that I'm not motivated to do anymore. 
    
    Anyway, check out the rest of my repos, I may be working on something cooler. Otherwise, thanks for everything friends. o7

--- BettyWhite.py
+++ BettyWhite.py
@@ -1,20 +1,61 @@
+'''
+Author: Logan Maupin
+Date: 09/19/2022
+
+FB Page Name: Daily Updates on Betty White's Health Condition
+FB page link: https://www.facebook.com/IsBettyOkOrNah
+
+GitHub link to this code: https://github.com/Voltaic314/Daily-FB-Poster-For-Python-Betty-White-Bot
+
+This program will post "She's Dead." to the FB page above. It does contain a function to use a random choice
+of 8 other possible statuses as well, but this isn't fleshed out as much as I'd like. So it gets old pretty quick.
+'''
+
 import requests
-import time
 import random
 import config
 
-page_id = "101862182407007"
 
-Possible_Status_List = ["She's Dead.", "Hey, guess what? She's dead.", "Ayo she dead tho fr fr.", "She's dead af.",
+## This function is no longer in use. However, I am keeping it here for legacy purposes,
+## in case I ever want to  use it again.
+def random_chosen_status():
+    '''
+    As stated above, this function is no longer used, but is still here just in case it is used later.
+    This function picks a random string from the list below, these strings would ideally be
+    posted as statuses in the post to fb function.
+    :returns: chosen_status variable which would be one out of the possible strings from the list below.
+    '''
+    Possible_Status_List = ["She's Dead.", "Hey, guess what? She's dead.", "Ayo she dead tho fr fr.", "She's dead af.",
                         "Dead.", "She's deadddddddddddddddd.", "Reports say that she is dead.",
                         "Unfortunately, she is dead.", "Dead, she is."]
-msg = random.choice(Possible_Status_List)
-post_url = 'https://graph.facebook.com/{}/feed'.format(page_id)
-payload = {
-    "message": msg,
-    "access_token": config.config_stuff['FB_Access_Token']
-  }
-r = requests.post(post_url, data=payload)
-print(r.text) # print the return text from FB servers to make sure the message went through properly or if not look at errors
+    chosen_status = random.choice(Possible_Status_List)
+    return chosen_status
 
-## This code was created by Logan Maupin aka Voltaic on GitHub. I am a student at the University of Arizona Online pursuing a degree in Applied Computing - Applied Artificial Intelligence. I plan on using my degree for cutting edge AI research. This project was not so much to help me with that goal, moreso just a fun side project, a good labor of love. Enjoy.
+def post_to_fb():
+    '''
+    This function's purpose is to make a web request to FB servers to post a status to the specified FB page.
+    :returns: print statement which contains the response variable with the text attribute. Basically
+    this is a return text from FB servers which contains the Post ID and Page ID within the response json.
+    '''
+    msg = "She's Dead."
+    page_id = "101862182407007"
+    post_url = 'https://graph.facebook.com/{}/feed'.format(page_id)
+    payload = {
+        "message": msg,
+        "access_token": config.config_stuff['FB_Access_Token']
+      }
+    response = requests.post(post_url, data=payload)
+    return print(response.text)
+
+def main():
+    '''
+    The purpose of this function is to define which functions should be run in order.
+    In this instance, there is only one function. So this is unnecessary, but I'm doing it anyway,
+    out of force of habit.
+    :returns: response.text variable from post_to_fb function
+    '''
+    post_to_fb()
+
+if __name__ == "__main__":
+    main()
+
