7d295a5aa1289033bfaef81ed8a75ec2ff713278
Voltaic314 <104940692+Voltaic314@users.noreply.github.com>
2022-11-11 20:39:45 -0500

    Fixing the format and documentation of this script
    Also moved the payload into its own build function so that we can make the code a bit more modular and testable here. :)

--- BettyWhite.py
+++ BettyWhite.py
@@ -1,4 +1,4 @@
-'''
+"""
 Author: Logan Maupin
 Date: 09/19/2022
 
@@ -9,34 +9,19 @@ GitHub link to this code: https://github.com/Voltaic314/Daily-FB-Poster-For-Pyth
 
 This program will post "She's Dead." to the FB page above. It does contain a function to use a random choice
 of 8 other possible statuses as well, but this isn't fleshed out as much as I'd like. So it gets old pretty quick.
-'''
+"""
 
 import requests
-import random
 import config
 
 
-## This function is no longer in use. However, I am keeping it here for legacy purposes,
-## in case I ever want to  use it again.
-def random_chosen_status():
-    '''
-    As stated above, this function is no longer used, but is still here just in case it is used later.
-    This function picks a random string from the list below, these strings would ideally be
-    posted as statuses in the post to fb function.
-    :returns: chosen_status variable which would be one out of the possible strings from the list below.
-    '''
-    Possible_Status_List = ["She's Dead.", "Hey, guess what? She's dead.", "Ayo she dead tho fr fr.", "She's dead af.",
-                        "Dead.", "She's deadddddddddddddddd.", "Reports say that she is dead.",
-                        "Unfortunately, she is dead.", "Dead, she is."]
-    chosen_status = random.choice(Possible_Status_List)
-    return chosen_status
+def build_fb_message():
+    """
+    This function builds up a dictionary object with specific data that we want inside the dictionary like what
+    we want to say in the post and our access token to allow access to post.
 
-def post_to_fb():
-    '''
-    This function's purpose is to make a web request to FB servers to post a status to the specified FB page.
-    :returns: print statement which contains the response variable with the text attribute. Basically
-    this is a return text from FB servers which contains the Post ID and Page ID within the response json.
-    '''
+    :returns: payload variable which represents the dictionary, and the post url to make the web request for.
+    """
     msg = "She's Dead."
     page_id = "101862182407007"
     post_url = 'https://graph.facebook.com/{}/feed'.format(page_id)
@@ -44,18 +29,36 @@ def post_to_fb():
         "message": msg,
         "access_token": config.config_stuff['FB_Access_Token']
       }
+    return payload, post_url
+
+
+def post_to_fb(payload, post_url):
+    """
+    The purpose of this function is to actually post our status to FB given the following 2 parameters. What to post
+    and where to post it.
+
+    :param payload: This is what we will post as well as our access token to give us permission to post it. see the
+    dictionary object in the build_fb_message function.
+    :param post_url: This is the fb feed url to send the post to. it must contain your page ID.
+    :returns: response with the .text attribute returned from FB servers after making the web request.
+    """
     response = requests.post(post_url, data=payload)
-    return print(response.text)
+    print(response.text)
+    return response.text
+
 
 def main():
-    '''
+    """
     The purpose of this function is to define which functions should be run in order.
-    In this instance, there is only one function. So this is unnecessary, but I'm doing it anyway,
-    out of force of habit.
-    :returns: response.text variable from post_to_fb function
-    '''
-    post_to_fb()
+
+    :returns: return_text variable from post_to_fb function which contains the post ID if there is a successful post.
+    """
+
+    payload, post_url = build_fb_message()
+    return_text = post_to_fb(payload, post_url)
+
+    return return_text
+
 
 if __name__ == "__main__":
     main()
-
